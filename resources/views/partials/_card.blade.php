90de93fe6d50d2c1ffb40a4984e2504ab0f01c95
plastikSun <marcom2k01@gmail.com>
2023-10-19 18:47:39 +0200

    add: show() route

--- resources/views/partials/_card.blade.php
+++ resources/views/partials/_card.blade.php
@@ -4,6 +4,11 @@
         <h5 class="card-title">{{ $comic->title }}</h5>
         <p class="card-text"><strong>Prezzo: </strong>{{ $comic->price }}$</p>
         <p class="card-text"><strong>Rilascio: </strong>{{ $comic->sale_date }}$</p>
-        <a href="#" class="btn btn-primary">Più informazioni</a>
+
+        <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+            <a href="{{ route('comics.show', $comic) }}" class="btn btn-primary">Visualizza</a>
+            <a href="#" class="btn btn-warning">Modifica</a>
+            <a href="#" class="btn btn-danger">Cancella</a>
+        </div>
     </div>
 </div>

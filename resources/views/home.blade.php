15daf727b1fcfbea971be05ed3c35694a8b77c15
plastikSun <marcom2k01@gmail.com>
2023-10-19 18:34:54 +0200

    add: @section('title')

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.app')
 
+@section('title', 'Homepage')
+
 @section('main-content')
-  <section class="container mt-5">
-    <h1>{{ $title }}</h1>
-  </section>
+    <section class="container mt-5">
+        <h1>{{ $title }}</h1>
+    </section>
 @endsection
